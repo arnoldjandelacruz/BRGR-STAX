800cfdd860dbf2aff4aadac74a116a599a387cd1
aceralph09 <aceralph.deramos@bicol-u.edu.ph>
2015-02-21 23:05:05 +0800

    shorter code. pwede pala to
    ngayon ko lang narealize

--- Sprites/one falling pepper.py
+++ Sprites/one falling pepper.py
@@ -17,104 +17,72 @@ clock = pygame.time.Clock()
 icon = pygame.image.load('pepper.png')
 pygame.display.set_icon(icon)
 
-pepImg = pygame.image.load('pepper.png')
-
-def pepper(x,y):
-    gameDisplay.blit(pepImg,(x,y))
-
-def things1(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things2(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things3(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things4(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things5(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things6(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things7(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things8(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things9(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
-
-def things10(thingx, thingy, thingw, thingh, color):
+def fall_pepper(thingx, thingy, thingw, thingh, color):
     pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
 def game_loop():
     x = (display_width * .5)
     y = (display_height)
 
-    thing_startx1 = random.randrange(0, display_width)
-    thing_y1 = random.randrange(-100, 0)
-    thing_speed1 = 7
-    thing_width1 = 5
-    thing_height1 = 5
+    x1 = random.randrange(0, display_width)
+    y1 = random.randrange(-100, 0)
+    s1 = 10
+    w1 = 5
+    h1 = 5
     
-    thing_startx2 = random.randrange(0, display_width)
-    thing_y2 = random.randrange(-100, 0)
-    thing_speed2 = 7
-    thing_width2 = 5
-    thing_height2 = 5
+    x2 = random.randrange(0, display_width)
+    y2 = random.randrange(-100, 0)
+    s2 = 10
+    w2 = 5
+    h2 = 5
 
-    thing_startx3 = random.randrange(0, display_width)
-    thing_y3 = random.randrange(-100, 0)
-    thing_speed3 = 7
-    thing_width3 = 5
-    thing_height3 = 5
+    x3 = random.randrange(0, display_width)
+    y3 = random.randrange(-100, 0)
+    s3 = 10
+    w3 = 5
+    h3 = 5
 
-    thing_startx4 = random.randrange(0, display_width)
-    thing_y4 = random.randrange(-100, 0)
-    thing_speed4 = 7
-    thing_width4 = 5
-    thing_height4 = 5
+    x4 = random.randrange(0, display_width)
+    y4 = random.randrange(-100, 0)
+    s4 = 10
+    w4 = 5
+    h4 = 5
 
-    thing_startx5 = random.randrange(0, display_width)
-    thing_y5 = random.randrange(-100, 0)
-    thing_speed5 = 7
-    thing_width5 = 5
-    thing_height5 = 5
+    x5 = random.randrange(0, display_width)
+    y5 = random.randrange(-100, 0)
+    s5 = 10
+    w5 = 5
+    h5 = 5
 
-    thing_startx6 = random.randrange(0, display_width)
-    thing_y6 = random.randrange(-400, -300)
-    thing_speed6 = 7
-    thing_width6 = 5
-    thing_height6 = 5
+    x6 = random.randrange(0, display_width)
+    y6 = random.randrange(-400, -300)
+    s6 = 10
+    w6 = 5
+    h6 = 5
     
-    thing_startx7 = random.randrange(0, display_width)
-    thing_y7 = random.randrange(-400, -300)
-    thing_speed7 = 7
-    thing_width7 = 5
-    thing_height7 = 5
+    x7 = random.randrange(0, display_width)
+    y7 = random.randrange(-400, -300)
+    s7 = 10
+    w7 = 5
+    h7 = 5
 
-    thing_startx8 = random.randrange(0, display_width)
-    thing_y8 = random.randrange(-400, -300)
-    thing_speed8 = 7
-    thing_width8 = 5
-    thing_height8 = 5
+    x8 = random.randrange(0, display_width)
+    y8 = random.randrange(-400, -300)
+    s8 = 10
+    w8 = 5
+    h8 = 5
 
-    thing_startx9 = random.randrange(0, display_width)
-    thing_y9 = random.randrange(-400, -300)
-    thing_speed9 = 7
-    thing_width9 = 5
-    thing_height9 = 5
+    x9 = random.randrange(0, display_width)
+    y9 = random.randrange(-400, -300)
+    s9 = 10
+    w9 = 5
+    h9 = 5
 
-    thing_startx10 = random.randrange(0, display_width)
-    thing_y10 = random.randrange(-400, -300)
-    thing_speed10 = 7
-    thing_width10 = 5
-    thing_height10 = 5
+    x0 = random.randrange(0, display_width)
+    y0 = random.randrange(-400, -300)
+    s0 = 10
+    w0 = 5
+    h0 = 5
 
     crashed = False
 
@@ -123,60 +91,59 @@ def game_loop():
             if event.type == pygame.QUIT:
                 crashed = True
     
-        pepper(x,y)
         gameDisplay.fill(white)
-        things1(thing_startx1, thing_y1, thing_width1, thing_height1, black)
-        things2(thing_startx2, thing_y2, thing_width2, thing_height2, black)
-        things3(thing_startx3, thing_y3, thing_width3, thing_height3, black)
-        things4(thing_startx4, thing_y4, thing_width4, thing_height4, black)
-        things5(thing_startx5, thing_y5, thing_width5, thing_height5, black)
-        things6(thing_startx6, thing_y6, thing_width6, thing_height6, black)
-        things7(thing_startx7, thing_y7, thing_width7, thing_height7, black)
-        things8(thing_startx8, thing_y8, thing_width8, thing_height8, black)
-        things9(thing_startx9, thing_y9, thing_width9, thing_height9, black)
-        things10(thing_startx10, thing_y10, thing_width10, thing_height10, black)
+        fall_pepper(x1, y1, w1, h1, black)
+        fall_pepper(x2, y2, w2, h2, black)
+        fall_pepper(x3, y3, w3, h3, black)
+        fall_pepper(x4, y4, w4, h4, black)
+        fall_pepper(x5, y5, w5, h5, black)
+        fall_pepper(x6, y6, w6, h6, black)
+        fall_pepper(x7, y7, w7, h7, black)
+        fall_pepper(x8, y8, w8, h8, black)
+        fall_pepper(x9, y9, w9, h9, black)
+        fall_pepper(x0, y0, w0, h0, black)
         
-        thing_y1 += thing_speed1
-        thing_y2 += thing_speed2
-        thing_y3 += thing_speed3
-        thing_y4 += thing_speed4
-        thing_y5 += thing_speed5
-        thing_y6 += thing_speed1
-        thing_y7 += thing_speed2
-        thing_y8 += thing_speed3
-        thing_y9 += thing_speed4
-        thing_y10 += thing_speed5
+        y1 += s1
+        y2 += s2
+        y3 += s3
+        y4 += s4
+        y5 += s5
+        y6 += s6
+        y7 += s7
+        y8 += s8
+        y9 += s9
+        y0 += s0
 
-        if thing_y1 > display_height:
-            thing_y1 = 0
-            thing_startx1 = random.randrange(0, display_width)
-        if thing_y2 > display_height:
-            thing_y2 = 0
-            thing_startx2 = random.randrange(0, display_width)
-        if thing_y3 > display_height:
-            thing_y3 = 0
-            thing_startx3 = random.randrange(0, display_width)
-        if thing_y4 > display_height:
-            thing_y4 = 0
-            thing_startx4 = random.randrange(0, display_width)
-        if thing_y5 > display_height:
-            thing_y5 = 0
-            thing_startx5 = random.randrange(0, display_width)
-        if thing_y6 > display_height:
-            thing_y6 = 0
-            thing_startx6 = random.randrange(0, display_width)
-        if thing_y7 > display_height:
-            thing_y7 = 0
-            thing_startx7 = random.randrange(0, display_width)
-        if thing_y8 > display_height:
-            thing_y8 = 0
-            thing_startx8 = random.randrange(0, display_width)
-        if thing_y9 > display_height:
-            thing_y9 = 0
-            thing_startx9 = random.randrange(0, display_width)
-        if thing_y10 > display_height:
-            thing_y10 = 0
-            thing_startx10 = random.randrange(0, display_width)
+        if y1 > display_height:
+            y1 = 0
+            x1 = random.randrange(0, display_width)
+        if y2 > display_height:
+            y2 = 0
+            x2 = random.randrange(0, display_width)
+        if y3 > display_height:
+            y3 = 0
+            x3 = random.randrange(0, display_width)
+        if y4 > display_height:
+            y4 = 0
+            x4 = random.randrange(0, display_width)
+        if y5 > display_height:
+            y5 = 0
+            x5 = random.randrange(0, display_width)
+        if y6 > display_height:
+            y6 = 0
+            x6 = random.randrange(0, display_width)
+        if y7 > display_height:
+            y7 = 0
+            x7 = random.randrange(0, display_width)
+        if y8 > display_height:
+            y8 = 0
+            x8 = random.randrange(0, display_width)
+        if y9 > display_height:
+            y9 = 0
+            x9 = random.randrange(0, display_width)
+        if y0 > display_height:
+            y0 = 0
+            x0 = random.randrange(0, display_width)
                     
         pygame.display.update()
         clock.tick(60)
