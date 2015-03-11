56feaf6b74a3a1ad062574a7e742772d484e30ce
aceralph09 <aceralph.deramos@bicol-u.edu.ph>
2015-03-11 09:30:45 +0800

    main menu

--- Sprites/main menu.py
+++ Sprites/main menu.py
@@ -18,34 +18,12 @@ light_green = (0,255,0)
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Start_Up')
 clock = pygame.time.Clock()
+bg = pygame.image.load('start-up menu.jpg')
 
-
-def game_intro():
-    #intro = False
-
-    #while not intro:
-        #for event in pygame.event.get():
-            #print(event)
-            #if event.type == pygame.QUIT:
-            #     pygame.quit()
-            #     quit()
-                 
-    gameDisplay.fill(white)
-    
-    mouse = pygame.mouse.get_pos()
-    if 150+100 > mouse[0] > 150 and 450+50 > mouse[1] > 450:
-        pygame.draw.rect(gameDisplay, light_green, (150,450,100,50))
-    else:
-        pygame.draw.rect(gameDisplay, green, (150,450,100,50))
-            
-
-    pygame.draw.rect(gameDisplay, red, (550,450,100,50))
-            
-    
+def backg():
     pygame.display.update()
-
-    clock.tick(15)
-
+    gameDisplay.blit(bg, (0,0))
+    pygame.display.update()
 def game_loop():
     x = (display_width * .5)
     y = (display_height)
@@ -61,7 +39,7 @@ def game_loop():
         pygame.display.update()
         clock.tick(60)
 
-game_intro()    
+backg()    
 game_loop()
 pygame.quit()
 quit()
